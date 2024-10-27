8753bba6a4177b8d00b24d0fe881e69d8e6516d3
Code with Pasindu <50491415+Pasindu123karu@users.noreply.github.com>
2024-10-28 03:44:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Make sure these paths match your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
